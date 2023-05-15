400592f27205d53327789f3cbc5f0b02517fd74e
Amine derbal <aminedoran31@gmail.com>
2023-05-15 13:34:44 +0100

    Modify files to fix linters errors

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,8 +1,7 @@
 require_relative 'decorator'
 
 class TrimmerDecorator < Decorator
-  
   def correct_name
-    super.length>10 ? super[0..9] : super   
+    super.length > 10 ? super[0..9] : super
   end
 end
