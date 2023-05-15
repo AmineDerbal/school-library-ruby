47fda0eb2bb159cd76ae5cbe2c7ab9a2e1a4cf3c
Amine derbal <aminedoran31@gmail.com>
2023-05-15 15:00:07 +0100

    add book file

---
+++ book.rb
@@ -0,0 +1,15 @@
+require_relative 'rentable'
+
+class Book
+  attr_accessor :title, :author
+  
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(date, person, book)
+    Rental.new(date, person, book)
+    end
+end
