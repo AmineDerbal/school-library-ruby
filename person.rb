0cebfd74c3a4d47f944c6a2375cec0c4f5c65855
Amine derbal <aminedoran31@gmail.com>
2023-05-15 13:31:23 +0100

    add inheritance to class in the file

--- person.rb
+++ person.rb
@@ -1,8 +1,13 @@
-class Person
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -13,9 +18,21 @@ class Person
     of_age? || @parent_permission == true
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+
+person = Person.new(22, 'maximilianus')
+  puts person.correct_name
+  capitalizedPerson = CapitalizeDecorator.new(person)
+  puts capitalizedPerson.correct_name
+  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+  puts capitalizedTrimmedPerson.correct_name
