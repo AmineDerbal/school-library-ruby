c467215645d7945ca895af749a3e2862255fef27
Amine derbal <aminedoran31@gmail.com>
2023-05-15 11:00:24 +0100

    Add student class

---
+++ Student.rb
@@ -0,0 +1,15 @@
+require './Person.rb'
+
+class Student < Person
+  def initialize(age, name = 'unknown', parent_permission = true, classroom )
+    super(age, name, parent_permission)
+    @classroom = classroom
+  
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
+end
+
