042e0be7e71aeaa6b54c2fa779b60c3f6ee96c29
SaneMyburg <sane.myburg@gmail.com>
2023-05-22 15:30:25 +0200

    Refactor app.rb

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require_relative './modules/add_person'
 require_relative './modules/add_rental'
 require_relative './modules/list_rentals'
 
+require_relative './modules/display_menu_options'
+
 class App
   include ListAllBooks
   include ListAllPeople
@@ -12,11 +14,42 @@ class App
   include AddBook
   include AddRental
   include ListAllRentals
+  include DisplayMenu
   attr_accessor :persons, :books, :rentals
 
   def initialize
     @persons = []
     @books = []
     @rentals = []
+  end
+
+  def run
+    loop do
+      display_menu_options
+      option = gets.chomp.to_i
+      puts "\n"
+      break if option == 7
+
+      process_option(option)
+    end
+  end
+
+  def process_option(option)
+    case option
+    when 1
+      list_books
+    when 2
+      list_people
+    when 3
+      add_person
+    when 4
+      add_book
+    when 5
+      add_rental
+    when 6
+      list_rentals
+    else
+      puts "Invalid option! Please choose a valid option.\n\n"
+    end
   end
 end
