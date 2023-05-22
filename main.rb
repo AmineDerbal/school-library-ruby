9769d1c43bfa96946b2e99d5eb7faf19bca7c83d
Amine derbal <aminedoran31@gmail.com>
2023-05-22 13:24:51 +0100

    fix bug

--- main.rb
+++ main.rb
@@ -14,19 +14,19 @@ end
 
 def run_app(app)
   menu_options = {
-    '1' => :list_book,
+    '1' => :list_books,
     '2' => :list_people,
-    '3' => :add_persons,
+    '3' => :add_person,
     '4' => :add_book,
-    '5' => :add_renTal,
+    '5' => :add_rental,
     '6' => :list_rentals,
     '7' => :exit
   }
 
   loop do
     display_menu_options
+    pry.binding
     input = gets.chomp
-    
     if menu_options.key?(input)
       action = menu_options[input]
       app.send(action)
