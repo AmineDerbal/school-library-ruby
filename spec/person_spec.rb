09a07ed4223681411341245a63bf6df0717aebdf
SaneMyburg <sane.myburg@gmail.com>
2023-05-24 14:32:36 +0200

    Fix linter errors

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,37 +3,33 @@ require_relative '../book'
 require_relative '../rental'
 
 
-describe 'test person class' do 
+describe 'test person class' do
   context 'instance of Person' do
-    person = Person.new(45,'amine')
-   
-    it 'takes two parameters and returns a Person object' do 
+    person = Person.new(45, 'amine')
+
+    it 'takes two parameters and returns a Person object' do
       expect(person).to be_instance_of(Person)
     end
- 
+
     it 'should have attributes' do
-      expect(person).to have_attributes(age: 45)  
-      expect(person).to have_attributes(name: 'amine')  
-      expect(person).to have_attributes(parent_permission: true)  
+      expect(person).to have_attributes(age: 45)
+      expect(person).to have_attributes(name: 'amine')
+      expect(person).to have_attributes(parent_permission: true)
     end
- 
+
     it 'the methods should work' do
       expect(person.can_use_services?).to be true
       expect(person.correct_name).to eq('amine')
-     end
-
+    end
   end
 
-  context 'use rental method' do 
-  person = Person.new(19,'david')
-  rental = Rental.new('4/13/2022',person,Book.new('Title','author'))
-  person.add_rental(Book.new('Title','author'),'4/13/2022')
+  context 'use rental method' do
+    person = Person.new(19, 'david')
+    rental = Rental.new('4/13/2022', person, Book.new('Title', 'author'))
+    person.add_rental(Book.new('Title', 'author'), '4/13/2022')
 
-  it 'Rentals array should contain the rental' do 
-    expect(person.rentals.include?(rental)).to eql true
+    it 'Rentals array should contain the rental' do
+      expect(person.rentals.include?(rental)).to eql true
+    end
   end
-  end
- 
- 
-
 end
